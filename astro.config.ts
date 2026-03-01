4513f8af8a49b35d4f405bc33ad2703669472368
Cameron Yee-Ping <cameronyeeping@gmail.com>
2026-02-28 17:38:58 -0800

    defineConfig

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,8 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: '/my-portfolio',
+  trailingSlash: "always", 
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
